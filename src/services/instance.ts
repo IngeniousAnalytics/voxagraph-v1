0f52632e07d339c8be2a1fae2e7cd678d4f0a37d
IngeniousAnalytics <hr@ingeniousanalytics.com>
2025-10-29 14:00:07 +0530

    backend api set

--- src/services/instance.ts
+++ src/services/instance.ts
@@ -5,8 +5,8 @@ import axios, { AxiosInstance } from 'axios';
 // Create instances
 const dashApiClient = axios.create({
   //baseURL: 'http://80.225.200.22:1312' // with docker image
-    baseURL: 'http://127.0.0.1:8000',
-   // baseURL: 'http://80.225.200.22/api' // without docker image
+   // baseURL: 'http://127.0.0.1:8000',
+    baseURL: 'http://80.225.200.22/api' // without docker image
 });
 
 // Function to get token from localStorage
